2ccd0fb40f851deed769f1e7d6eb8529a024f7b1
momin-mostafa <mominmostafa.g@gmail.com>
2023-07-08 01:31:01 +0600

    dirty branch

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:ecom_frontend/app/data/common/api_response.dart';
+import 'package:ecom_frontend/app/data/helper/check_api.dart';
 import 'package:ecom_frontend/app/modules/home/model/product.dart';
 import 'package:ecom_frontend/app/modules/home/provider/home_provider.dart';
-import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
@@ -51,16 +51,5 @@ class HomeController extends GetxController {
     });
     // listOfProduct.value = temp;
     listOfProduct.value = temp;
-  }
-
-  checkApi(ApiResponse apiResponse, Function addFunction) {
-    if (apiResponse.response != null &&
-        apiResponse.response!.statusCode == 200) {
-      debugPrint(apiResponse.response!.body);
-      addFunction();
-    } else {
-      debugPrint("App DEBUG ==> ${apiResponse.response?.reasonPhrase}");
-      Get.snackbar('APP Debug', '${apiResponse.response?.reasonPhrase}');
-    }
   }
 }
--- lib/app/modules/mens/controllers/mens_controller.dart
+++ lib/app/modules/mens/controllers/mens_controller.dart
@@ -1,3 +1,62 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:ecom_frontend/app/modules/home/model/product.dart';
 import 'package:get/get.dart';
 
-class MensController extends GetxController {}
+import '../../../data/common/api_response.dart';
+import '../../../data/helper/check_api.dart';
+import 'package:http/http.dart' as http;
+
+class MensController extends GetxController {
+  RxList<Product> listOfProduct = <Product>[].obs;
+  RxList<Product> listOfMaleProduct = <Product>[].obs;
+
+  @override
+  void onReady() {
+    getAllProduct();
+    super.onReady();
+  }
+
+  Future getAllProduct() async {
+    List<Product> temp = [];
+    // Product? temp;
+    ApiResponse apiResponse = await testCallToLocal();
+
+    checkApi(apiResponse, () {
+      try {
+        if (apiResponse.response != null) {
+          var data = jsonDecode(apiResponse.response!.body);
+          for (var element in data) {
+            temp.add(Product.fromJson(element));
+          }
+        }
+      } catch (e) {
+        log(e.toString(), name: 'getAllProduct HomeController');
+      }
+    });
+    listOfProduct.value = temp;
+    filterForMen();
+  }
+
+  filterForMen() {
+    List<Product> temp = [];
+    for (var element in listOfProduct) {
+      if (element.category == 'm') {
+        temp.add(element);
+      }
+    }
+    listOfMaleProduct.value = temp;
+    update();
+  }
+
+  testCallToLocal() async {
+    var response = await http.get(
+        Uri.parse('http://127.0.0.1:8000/api/product-list/'),
+        headers: {'Content-Type': 'application/json', 'Accept': '*/*'});
+    if (response.statusCode == 200) {
+      return ApiResponse.withSuccess(response);
+    }
+    return ApiResponse.fromError("Response not 200");
+  }
+}
